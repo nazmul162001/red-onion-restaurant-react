130b88d8b41a460c0b29efbfb4d0b8c5f824116c
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-15 20:45:26 +0600

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
     <div className="">
       <Header></Header>
+      <HomePage></HomePage>
     </div>
   );
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h3>This is homePage</h3>
-    </div>
+    <section className='hero flex justify-center items-center text-center'>
+      <div className="hero-content">
+        <h1 className="hero-title text-4xl py-5 font-semibold text-gray-700">
+          Best Food waiting for your belly
+        </h1>
+        <div className="search-field relative">
+        <input className='w-[65%] py-2 pl-3 rounded-full' type="search" name="search" id="serch" placeholder='Search food items' />
+        <button className='absolute bg-red-500 py-2 text-white px-12 rounded-full right-16'>Search</button>
+        </div>
+      </div>
+    </section>
   );
 };
 
